3cd21b4242c9d8ddf7af22a34f693a9b9ef3a2da
Stefan Hoffmann <stho32github@gmail.com>
2022-11-22 17:51:13 +0100

    Basic sending and receiving works

---
+++ Source/LocalNetAppChat/LocalNetAppChat.ConsoleClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Specialized;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using LocalNetAppChat.Domain;
+
+// using (WebClient client = new WebClient())
+// {
+//     var result = client.DownloadString("http://localhost:5000/receive");
+//     Console.WriteLine(result);
+// }
+
+
+using (HttpClient client = new HttpClient())
+{
+    Message message = new Message(
+        Guid.NewGuid().ToString(),
+        "Lukas",
+        "Hi ich bin Lukas",
+        Array.Empty<string>(),
+        true,
+        "Message"
+    );
+
+    var result = await client.PostAsJsonAsync("http://localhost:5000/send", message);
+    
+    Console.WriteLine(result);
+}
+
--- Source/LocalNetAppChat/LocalNetAppChat.Server/Program.cs
+++ Source/LocalNetAppChat/LocalNetAppChat.Server/Program.cs
@@ -1,6 +1,29 @@
+using System.Collections.Concurrent;
+using System.Text.Json;
+using LocalNetAppChat.Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var messages = new List<Message>();
+messages.Add(new Message(
+    Guid.NewGuid().ToString(),
+    "Joe",
+    "This is some text",
+    Array.Empty<string>(),
+    true,
+    "Message"));
+    
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/receive", () =>
+{
+    Console.WriteLine("Receiving!");
+    return JsonSerializer.Serialize(messages);
+});
+
+app.MapPost("/send", (Message message) =>
+{
+    Console.WriteLine(message);
+});
 
 app.Run();
