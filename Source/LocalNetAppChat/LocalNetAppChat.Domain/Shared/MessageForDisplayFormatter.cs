5a60c14ad043d8f4c917148fc3df3ddeb7f20ae8
naseif <d2mds21@gmail.com>
2022-12-19 18:49:06 +0100

    Check if the message is private and change the output message accordingly

--- Source/LocalNetAppChat/LocalNetAppChat.Domain/Shared/MessageForDisplayFormatter.cs
+++ Source/LocalNetAppChat/LocalNetAppChat.Domain/Shared/MessageForDisplayFormatter.cs
@@ -4,6 +4,10 @@ public static class MessageForDisplayFormatter
 {
     public static string GetTextFor(ReceivedMessage message)
     {
+        if (!string.IsNullOrWhiteSpace(message.Receiver))
+        {
+            return ($" - [{message.Timestamp:yyyy-MM-dd HH:mm:ss}] *PRIVATEMSG* {message.Message.Name}: {message.Message.Text}");
+        }
         return ($" - [{message.Timestamp:yyyy-MM-dd HH:mm:ss}] {message.Message.Name}: {message.Message.Text}");
     }
 }
